b49d17966a4907c6700ae6707dbde2094e97e1a7
Abhishek Kumar <abhishek22512@gmail.com>
2023-08-16 16:20:36 +0530

    Updated test_url_caching to test on I/O data

--- test_main.py
+++ test_main.py
@@ -66,10 +66,176 @@ def test_url_caching(empty_deployment):
     # Make empty_deployment appear used to silence pylint warning
     assert empty_deployment is None
 
-    pub_client = kcidb.mq.URLListPublisher(
+    client = kcidb.Client(
         project_id=os.environ["GCP_PROJECT"],
-        topic_name=os.environ["KCIDB_UPDATED_URLS_TOPIC"]
+        topic_name=os.environ["KCIDB_LOAD_QUEUE_TOPIC"]
     )
+
+    data = {
+        "version": {
+            "major": 4,
+            "minor": 0
+        },
+        "checkouts": [
+            {
+                "contacts": [
+                    "rdma-dev-team@redhat.com"
+                ],
+                "start_time": "2020-03-02T15:16:15.790000+00:00",
+                "git_repository_branch": "wip/jgg-for-next",
+                "git_commit_hash": "5e29d1443c46b6ca70a4c940a67e8c09f05dcb7e",
+                "patchset_hash": "",
+                "git_repository_url": "git://git.kernel.org/pub/scm/linux/kernel/git/rdma/rdma.git",
+                "misc": {
+                    "pipeline_id": 467715
+                },
+                "id": "non_test:1",
+                "origin": "non_test",
+                "patchset_files": [],
+                "valid": True,
+            },
+            {
+                "contacts": [
+                    "rdma-dev-team@redhat.com"
+                ],
+                "start_time": "2020-03-02T15:16:15.790000+00:00",
+                "git_repository_branch": "wip/jgg-for-next",
+                "git_commit_hash": "1254e88b4fc1470d152f494c3590bb6a33ba0bab",
+                "patchset_hash": "",
+                "git_repository_url": "git://git.kernel.org/pub/scm/linux/kernel/git/rdma/rdma.git",
+                "misc": {
+                    "pipeline_id": 467715
+                },
+                "id": "test:1",
+                "origin": "test",
+                "patchset_files": [],
+                "valid": True,
+            },
+        ],
+        "builds": [
+            {
+                "architecture": "aarch64",
+                "command": "make -j30 INSTALL_MOD_STRIP=1 targz-pkg",
+                "compiler": "aarch64-linux-gnu-gcc (GCC) 9.2.1 20190827 (Red Hat Cross 9.2.1-1)",
+                "config_name": "fedora",
+                "duration": 237.0,
+                "id": "non_test:1",
+                "origin": "non_test",
+                "input_files": [],
+                "log_url": "https://github.com/kernelci/kcidb/blob/main/requirements.txt",
+                "misc": {
+                    "job_id": 678223,
+                    "pipeline_id": 469720
+                },
+                "output_files": [],
+                "checkout_id": "test:1",
+                "start_time": "2020-03-03T17:52:02.370000+00:00",
+                "valid": True
+            },
+            {
+                "architecture": "aarch64",
+                "command": "make -j30 INSTALL_MOD_STRIP=1 targz-pkg",
+                "compiler": "aarch64-linux-gnu-gcc (GCC) 9.2.1 20190827 (Red Hat Cross 9.2.1-1)",
+                "config_name": "fedora",
+                "duration": 237.0,
+                "id": "test:1",
+                "origin": "test",
+                "input_files": [],
+                "log_url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/03/469720/build_aarch64.log",
+                "misc": {
+                    "job_id": 678223,
+                    "pipeline_id": 469720
+                },
+                "output_files": [],
+                "checkout_id": "test:1",
+                "start_time": "2020-03-03T17:52:02.370000+00:00",
+                "valid": True
+            },
+        ],
+        "tests": [
+            {
+                "build_id": "non_test:1",
+                "comment": "IOMMU boot test",
+                "duration": 1847.0,
+                "id": "non_test:1",
+                "origin": "non_test",
+                "output_files": [
+                    {
+                        "name": "x86_64_4_console.log",
+                        "url": "https://github.com/kernelci/kcidb/blob/main/README.md"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_dmesg.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_dmesg.log"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_resultoutputfile.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_resultoutputfile.log"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_taskout.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_taskout.log"
+                    }
+                ],
+                "environment": {
+                    "comment": "meson-gxl-s905d-p230 in lab-baylibre",
+                    "misc": {
+                        "device": "meson-gxl-s905d-p230",
+                        "instance": "meson-gxl-s905d-p230-sea",
+                        "lab": "lab-baylibre",
+                        "mach": "amlogic",
+                        "rootfs_url": "https://storage.kernelci.org/images/rootfs/buildroot/kci-2019.02-9-g25091c539382/arm64/baseline/rootfs.cpio.gz"
+                    }
+                },
+                "path": "redhat_iommu_boot",
+                "start_time": "2020-03-04T21:30:57+00:00",
+                "status": "ERROR",
+                "waived": True
+            },
+            {
+                "build_id": "test:1",
+                "comment": "IOMMU boot test",
+                "duration": 1847.0,
+                "id": "test:1",
+                "origin": "test",
+                "output_files": [
+                    {
+                        "name": "x86_64_4_console.log",
+                        "url": "https://github.com/kernelci/kcidb/blob/main/setup.py"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_dmesg.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_dmesg.log"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_resultoutputfile.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_resultoutputfile.log"
+                    },
+                    {
+                        "name": "x86_64_4_IOMMU_boot_test_taskout.log",
+                        "url": "https://cki-artifacts.s3.amazonaws.com/datawarehouse/2020/03/04/471145/x86_64_4_IOMMU_boot_test_taskout.log"
+                    }
+                ],
+                "environment": {
+                    "comment": "meson-gxl-s905d-p230 in lab-baylibre",
+                    "misc": {
+                        "device": "meson-gxl-s905d-p230",
+                        "instance": "meson-gxl-s905d-p230-sea",
+                        "lab": "lab-baylibre",
+                        "mach": "amlogic",
+                        "rootfs_url": "https://storage.kernelci.org/images/rootfs/buildroot/kci-2019.02-9-g25091c539382/arm64/baseline/rootfs.cpio.gz"
+                    }
+                },
+                "path": "redhat_iommu_boot",
+                "start_time": "2020-03-04T21:30:57+00:00",
+                "status": "ERROR",
+                "waived": True
+            },
+        ],
+    }
+    
+    # Submit data to submission queue
+    client.submit(data)
 
     # Submit messages with different URLs
     urls_messages = [
@@ -77,9 +243,6 @@ def test_url_caching(empty_deployment):
         ["https://github.com/kernelci/kcidb/blob/main/requirements.txt",
          "https://github.com/kernelci/kcidb/blob/main/README.md"]
     ]
-
-    for urls in urls_messages:
-        pub_client.publish(urls)
 
     # Retry checking URLs in the cache for a minute
     retry_interval = 5  # seconds
